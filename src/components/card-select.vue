e96ae101201ea60d49816b6c61d0dcd3b9071658
Dmitriy Tikhanenkov <TikhanenkovD@gmail.com>
2020-10-21 19:55:14 +0300

    init

---
+++ src/components/card-select.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="select">
+    <select class="select__input" v-model="expiresM">
+        <option value="" disabled selected>Month</option>
+        <option v-for="n in 12" :value="n < 10 ? '0' + n : n" :key="n">
+            {{ 10 > n ? "0" + n : n }}
+        </option>
+    </select>
+</div>
+</template>
+
+<script>
+export default {
+    Name: 'card-select'
+};
+</script>
+
+<style>
+</style>
--- src/main.js
+++ src/main.js
@@ -2,3 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+
+let ghpages = require('gh-pages');
+ 
+ghpages.publish('dist', function(err) {
+    console.log(err);
+});
