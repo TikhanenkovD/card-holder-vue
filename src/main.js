0da884bd6d5e59794fa5286461022467ff6ea174
Dmitriy Tikhanenkov <TikhanenkovD@gmail.com>
2020-10-21 20:59:06 +0300

    github

--- src/main.js
+++ src/main.js
@@ -2,10 +2,3 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
-
-
-let ghpages = require('gh-pages');
- 
-ghpages.publish('dist', function(err) {
-    console.log(err);
-});
